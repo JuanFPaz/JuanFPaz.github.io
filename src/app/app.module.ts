0805bb761203e13ecfcf7803fc8dc5362708f880
JuanFPaz <juanf.paz94@gmail.com>
2022-09-20 03:07:36 -0300

    second commit xd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
