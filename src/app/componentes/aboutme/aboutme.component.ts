ce3f3ce78b208374ee00179f4eea00bc7e66afcf
JuanFPaz <juanf.paz94@gmail.com>
2022-09-30 18:41:57 -0300

    Arregle los GPS del modal en AboutMe.

--- src/app/componentes/aboutme/aboutme.component.ts
+++ src/app/componentes/aboutme/aboutme.component.ts
@@ -43,7 +43,7 @@ export class AboutmeComponent implements OnInit {
     this.imgAltModal = "El Roca papá";
     this.parrafo1 = "Colegio Secundario ubicado en el Barrio de Belgrano, Capital Federal.";
     this.parrafo2 = "Realice los 5 años del secundario en este colegio, y el titulo que recibí es de Bachiller.";
-    this.gpsmodal = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3286.8614577146072!2d-58.52221598423058!3d-34.53173786120124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb0d1e28cdb6b%3A0xae7580bb6de7116a!2sCentro%20Universitario%20Vicente%20L%C3%B3pez!5e0!3m2!1ses!2sar!4v1661660615981!5m2!1ses!2sar";
+    this.gpsmodal = "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13141.488109793312!2d-58.45935500000001!3d-34.569452000000005!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6e541577115686aa!2sColegio%20N%C2%B0%2008%20Julio%20Argentino%20Roca!5e0!3m2!1ses!2sar!4v1661660668313!5m2!1ses!2sar";
     this.direccionmodal = "Dirección: Carlos E. Zuberbuhler 1850."
     this.localidadmodal = "Localidad: Belgrano, Capital Federal. Argentina."
   }
@@ -63,7 +63,7 @@ export class AboutmeComponent implements OnInit {
     this.imgAltModal = "BBQ";
     this.parrafo1 = "Restaurante de comida China y Coreana ubicado en el barrio de Belgrano, o conocido popularmente como el barrio Chino.";
     this.parrafo2 = "Comencé como ayudante de cocina elaborando los platos mas sencillos y de la limpieza e higiene del lugar. Conseguí ganarme un puesto como cocinero tras la salida del chef, no sin antes haber aprendido todo de el y dejarme listo para ese ocupar su lugar.";
-    this.gpsmodal = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.7387573007136!2d-58.37803618422862!3d-34.61076696537923!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccad475ccbbc1%3A0xaf7af7e0de859844!2sMinisterio%20de%20Desarrollo%20Productivo%20de%20la%20Naci%C3%B3n!5e0!3m2!1ses!2sar!4v1661660720386!5m2!1ses!2sar";
+    this.gpsmodal = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3285.833794650115!2d-58.45157958422992!3d-34.557763362576246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5cd256e25d1%3A0x16c2c102d9b655e7!2sBBQ%20Town!5e0!3m2!1ses!2sar!4v1661726372558!5m2!1ses!2sar";
     this.direccionmodal = "Dirección: Av. Juramento 1656."
     this.localidadmodal = "Localidad: Belgrano, Capital Federal. Argentina."
   }
@@ -83,7 +83,7 @@ export class AboutmeComponent implements OnInit {
     this.imgAltModal = "Cinemark Hoyts";
     this.parrafo1 = "El Cinemark Hoyts fue mi primer trabajo, ubicado en el Shopping Dot Baires del barrio de Saavedra.";
     this.parrafo2 = "Los puestos de trabajo eran rotativos, pero generalmente me encontraba en el sector de las salas, realizando tareas de limpieza, mantenimiento, recibiendo a los clientes con sus respectivos boletos, guiarlos hasta sus respectivas salas y asientos, etc.";
-    this.gpsmodal = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.7387573007136!2d-58.37803618422862!3d-34.61076696537923!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccad475ccbbc1%3A0xaf7af7e0de859844!2sMinisterio%20de%20Desarrollo%20Productivo%20de%20la%20Naci%C3%B3n!5e0!3m2!1ses!2sar!4v1661660720386!5m2!1ses!2sar";
+    this.gpsmodal = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3286.2961842861996!2d-58.49354076863638!3d-34.54605549949347!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb6c10694d555%3A0x88e59ff0d192bc!2sCinemark%20Hoyts!5e0!3m2!1ses!2sar!4v1661726248499!5m2!1ses!2sar";
     this.direccionmodal = "Dirección: Vedia 3626."
     this.localidadmodal = "Localidad: Saavedra, Capital Federal. Argentina."
   }
